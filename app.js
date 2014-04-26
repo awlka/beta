4c093ce6158caed38e2ea1a31d3014d943a5d344
Lucas Churchill <lucas@agtlucas.com>
2014-04-26 17:00:15 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var express = require('express');
+var favicon = require('static-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var path = require('path');
+
+var app = express();
+
+// Routes
+var home = require('./routes/index');
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('layout', 'layout');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public/build')));
+
+app.use('/', home);
+
+// 404
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Errors handlers
+
+// Development error handler
+// Will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler
+// No stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
